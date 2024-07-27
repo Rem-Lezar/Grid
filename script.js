f391dbdf35ce57a63c0333fd7ea84ec6aa819858
Rem-Lezar <joshdotdillow@gmail.com>
2024-07-27 16:18:22 -0400

    Update script.js
    Code cleanup/organization. Small tweaks to try and get the coin display to work.

--- script.js
+++ script.js
@@ -1,58 +1,75 @@
+//Executes contained code once at launch. ?initializes other event listeners?
 document.addEventListener('DOMContentLoaded', () => {
-    // Code that executes once at startup
 	
-	//const --> Var declaration, Never Changes
-	//let --> Var declaration, Can change
-	
-	let currency_coins = 0 //NOT DOING ANYTHING WITH THIS YET
-	
+	// NOTES SECTION
+	//========================================
+	const --> Var declaration, Never Changes
+	let --> Var declaration, Can change
+	//=========================================
+	// INIT DECLARATIONS
+	//========================================
 	const cube = document.getElementById('cube');
-    let cube_position = { x: 2, y: 2 }; //start on coords  (2,2)
+    	let cube_position = { x: 2, y: 2 }; //start on coords  (2,2)
 	
 	const coin = document.getElementById('coin');
-	let coin_position = { x: getRandomInt(11), y: getRandomInt(11) }; //11 is actually 12 due to floor in function.
+	let coin_position = { x: getRandomInt(12), y: getRandomInt(12) }; // TESTING 12 (prev:11) contingent on func.
 	
-	coin.style.gridColumnStart = coin_position.x;
-	coin.style.gridRowStart =coin_position.y;
-	
-	function getRandomInt(max) {
+	let currency_coins = 0
+	//========================================
+	// INIT FUNCTION DELCARATIONS
+	//========================================
+	function getRandomInt(max-1) {
 		return Math.floor(Math.random() * max) + 1;
 	}
 
 	function checkOverlap() {
 		return cube_position.x === coin_position.x && cube_position.y === coin_position.y;
 	}
+	//========================================
+	// INIT GAME SETUP
+	//========================================
+	//
+	coin.style.gridColumnStart = coin_position.x;
+	coin.style.gridRowStart =coin_position.y;
+	//
+	coin_display.textContent = currency_coins;
+	coin_display.style.gridColumn = 1;
+	coin_display.style.gridRow = 1;
+	//========================================
+
 	
 	
-	// Event listener that ticks when arrowkeys are pressed
-    document.addEventListener('keydown', (event) => {
-        switch (event.key) {
-            case 'ArrowUp':
-                if (cube_position.y > 1) cube_position.y--;
-                break;
-            case 'ArrowDown':
-                if (cube_position.y < 12) cube_position.y++;
-                break;
-            case 'ArrowLeft':
-                if (cube_position.x > 1) cube_position.x--;
-                break;
-            case 'ArrowRight':
-                if (cube_position.x < 12) cube_position.x++;
-                break;
-        }
-        cube.style.gridColumnStart = cube_position.x;
-        cube.style.gridRowStart = cube_position.y;
+	// Event listener -- executes code when arrowkey is pressed (or held)
+	document.addEventListener('keydown', (event) => {
+		// CUBE MOVEMENT CONTROLLER
+		switch (event.key) {
+			case 'ArrowUp':
+                		if (cube_position.y > 1) cube_position.y--;
+                		break;
+            		case 'ArrowDown':
+                		if (cube_position.y < 12) cube_position.y++;
+                		break;
+            		case 'ArrowLeft':
+                		if (cube_position.x > 1) cube_position.x--;
+                		break;
+            		case 'ArrowRight':
+                		if (cube_position.x < 12) cube_position.x++;
+                		break;
+        	}
+		cube.style.gridColumn = cube_position.x;
+		cube.style.gridRow = cube_position.y;
 		
+		// CUBE AND COIN OVERLAP (POST MOVEMENT) CONTROLLER
 		if (checkOverlap()) {
-			coin_position = { x: getRandomInt(11), y: getRandomInt(11) };
+			coin_position = { x: getRandomInt(12), y: getRandomInt(12) }; // TESTING 12 (pre:11) CONTINGENT ON FUNC
 			coin.style.gridColumnStart = coin_position.x;
 			coin.style.gridRowStart = coin_position.y;
 			
 			currency_coins++;
-			console.log(currency_coins)
+			console.log(currency_coins) // DEBUGGING STUFF LIKE IF THE COIN CHOOSES THE PLAYER POSITION & UNFORSEEN THINGS
 			coin_display.textContent = currency_coins;
-			coin_display.style.gridColumnStart = coin_position.x;
-			coin_display.style.gridRowStart = coin_position.y;
-        }
-    });
+			coin_display.style.gridColumn = 1;
+			coin_display.style.gridRow = 1;
+		}
+	});
 });
